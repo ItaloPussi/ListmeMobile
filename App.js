8138fb10ad38f75528aca9bea5d97bcd03d66dba
ItaloPussi <italopussi@gmail.com>
2021-02-20 10:48:16 -0300

    Base todolist created (The net ninja)

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import AddTodo from './components/AddTodo';
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {text: "Buy coffee", key: "1"},
+    {text: "Create an app", key:"2"},
+    {text: "Play on the switch", key: "3"}
+  ])
+
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos)=> {
+      return [
+        ...prevTodos,
+        {
+          text,
+          key: Math.random().toString()
+        }
+
+      ]
+    })
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList 
+              data ={todos}
+              renderItem={({item})=> (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+      </View>
     </View>
   );
 }
@@ -15,7 +51,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    padding: 40,
+    flex: 1,
+  },
+  list: {
+    marginTop: 20,
+    flex: 1,
   },
 });
